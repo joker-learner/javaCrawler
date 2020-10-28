a1c802b6405ddb80eb930101313331f96f8001d2
joker-learner <lc19970225>
2020-10-28 15:38:14 +0800

    3rdCommit

--- src/main/java/com/lc/Application.java
+++ src/main/java/com/lc/Application.java
@@ -4,13 +4,14 @@ package com.lc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableScheduling
-//@ComponentScan(basePackages = {"com.lc.controller" , "com.lc.dao"})
-//@EntityScan("com.lc.pojo")
+@ComponentScan(basePackages = {"com.lc.controller" , "com.lc.dao"})
+@EntityScan("com.lc.pojo")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/lc/controller/MyController.java
+++ src/main/java/com/lc/controller/MyController.java
@@ -1,8 +1,17 @@
 package com.lc.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@Service
 public class MyController {
+
+    @GetMapping ("/save")
+    public String save(){
+        System.out.println("你是猪");
+        return "你好...";
+    }
 
 }
